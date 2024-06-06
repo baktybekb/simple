84e6c1d91379846902015ddc4b91f62204eb2061
baktybek_baiserkeev <bakbbz61@gmail.com>
2024-06-06 08:15:19 +0600

    final

--- function_app/__init__.py
+++ function_app/__init__.py
@@ -1,6 +1,14 @@
+import os
 import logging
+import datetime
+from dotenv import load_dotenv
 import azure.functions as func
 
+# Load environment variables from .env file
+load_dotenv()
+
+
 def main(mytimer: func.TimerRequest) -> None:
-    logging.info('Python timer trigger function ran.')
-    logging.info('BAHAAAAAAAAAAAAAAAAAAAA')
+    name = os.getenv('NAME')
+    utc_timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    logging.info(f'Python timer trigger function ran at {utc_timestamp}. Name: {name}')
